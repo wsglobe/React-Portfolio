c2e778c47f4787b111a78861a0741cbb6a79549c
Walter Wang <wsglobe@gmail.com>
2020-08-17 21:24:12 -0500

    added structure

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Wrapper from "./components/Wrapper/Wrapper";
 import './App.css';
 
 function App() {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} handleFormSubmit={handleFormSubmit}/>
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+    
 
 export default App;
