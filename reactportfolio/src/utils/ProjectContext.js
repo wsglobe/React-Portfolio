20903780186b75d8c510f416faf8009dc6cccfdc
Walter Wang <wsglobe@gmail.com>
2020-08-17 21:44:45 -0500

    added context API

---
+++ reactportfolio/src/utils/ProjectContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProjectContext = React.createContext({
+  id: "",
+  key: "",
+  title: "",
+  caption: "",
+  image: "",
+  url: "",
+});
+
+export default ProjectContext;
