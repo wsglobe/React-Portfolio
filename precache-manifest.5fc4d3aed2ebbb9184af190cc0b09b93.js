63936964b35401b0dbbb03efd4cbd678291c5444
Walter Wang <wsglobe@gmail.com>
2020-08-19 23:14:56 -0500

    Updates

---
+++ precache-manifest.5fc4d3aed2ebbb9184af190cc0b09b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3a2e3aae799998abd307edbb211c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e8d3cc0c3d57b636a12",
+    "url": "/static/css/main.51a971c0.chunk.css"
+  },
+  {
+    "revision": "570cc23db0486b47a029",
+    "url": "/static/js/2.f3d8464f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3d8464f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8d3cc0c3d57b636a12",
+    "url": "/static/js/main.cbfd10ef.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.58fcdcc04e12c82fa8c4ade1bbf7a4aa.js"
+  "/precache-manifest.5fc4d3aed2ebbb9184af190cc0b09b93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
